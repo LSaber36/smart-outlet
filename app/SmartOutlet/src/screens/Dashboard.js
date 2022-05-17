b34a557c7f8e8270d51af2ea086e6e84b8ff9c26
Lucas Saber <L.Saber36@gmail.com>
2022-05-17 19:06:03 -0400

    Added scrollview for devices

--- app/SmartOutlet/src/screens/Dashboard.js
+++ app/SmartOutlet/src/screens/Dashboard.js
@@ -1,35 +1,81 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { styles, colors } from '../styles';
-import { Button } from 'react-native-elements';
+import { Button, ListItem } from 'react-native-elements';
 
 export const Dashboard = ({ navigation }) => {
-	const {	container, fullWidthHeight, buttonContainer } = styles;
-	const { textStyle, navButtonView, buttonStyle, deviceButtonView } = dashboardStyles;
+	const {	container, fullWidthHeight, buttonContainer, center } = styles;
+	const {
+		textStyle,
+		navButtonView,
+		buttonStyle,
+		addDeviceButtonView,
+		scrollViewContainer,
+		scrollViewStyle,
+		deviceItemStyle,
+		deviceItemContainer,
+		contentStyle,
+		itemTextStyle
+	} = dashboardStyles;
+
+	// Use a static list for now, but use a dynamic one later
+	const items = [
+		{ value: 'Device', key: 1 },
+		{ value: 'Device', key: 2 },
+		{ value: 'Device', key: 3 },
+		{ value: 'Device', key: 4 },
+		{ value: 'Device', key: 5 },
+		{ value: 'Device', key: 6 },
+		{ value: 'Device', key: 7 },
+		{ value: 'Device', key: 8 },
+		{ value: 'Device', key: 9 },
+		{ value: 'Device', key: 10 },
+		{ value: 'Device', key: 11 },
+		{ value: 'Device', key: 12 }
+	];
 
 	return (
 		<View style = { container }>
-			<Text style = { textStyle }> Dashboard Page </Text>
-			<View style = { deviceButtonView }>
-				<Button
-					title = 'Device 1'
-					containerStyle = { [buttonContainer, buttonStyle] }
-					buttonStyle = { [fullWidthHeight] }
-					onPress = { () => { navigation.navigate('Device') } }
-				/>
-			</View>
 			<View style = { navButtonView }>
 				<Button
 					title = '?'
 					containerStyle = { [buttonContainer, buttonStyle] }
 					buttonStyle = { fullWidthHeight }
 				/>
-
 				<Button
 					title = 'Settings'
 					containerStyle = { [buttonContainer, buttonStyle] }
 					buttonStyle = { fullWidthHeight }
 					onPress = { () => { navigation.navigate('Settings') } }
+				/>
+			</View>
+			<Text style = { textStyle }> Dashboard Page </Text>
+			<View style = { [center, scrollViewContainer] }>
+				<ScrollView style = { scrollViewStyle }>
+					{
+						items.map((item, i) => (
+							<ListItem
+								key = { i }
+								containerStyle = { deviceItemContainer }
+							>
+								<ListItem.Content style = { contentStyle }>
+									<ListItem.Title style = { itemTextStyle }>
+										{ item.value } { item.key }
+									</ListItem.Title>
+								</ListItem.Content>
+							</ListItem>
+						))
+					}
+				</ScrollView>
+			</View>
+			<View style = { addDeviceButtonView }>
+				<Button
+					title = 'Add a device'
+					containerStyle = { [buttonContainer, buttonStyle] }
+					buttonStyle = { [fullWidthHeight] }
+					onPress = { () => {
+
+					} }
 				/>
 			</View>
 		</View>
@@ -40,25 +86,49 @@ const dashboardStyles = {
 	textStyle: {
 		color: colors.dark,
 		fontSize: 40,
-		marginTop: '65%'
+		marginTop: '5%'
 	},
-	deviceButtonView: {
+	addDeviceButtonView: {
 		height: '10%',
 		width: '80%',
-		marginTop: '20%',
+		marginTop: '5%',
 		justifyContent: 'space-around',
 		alignItems: 'center'
 	},
 	navButtonView: {
 		height: '10%',
 		width: '80%',
-		marginTop: '20%',
+		marginTop: '5%',
 		flexDirection: 'row',
 		justifyContent: 'space-around',
 		alignItems: 'center'
 	},
 	buttonStyle: {
-		width: '35%',
+		width: '40%',
 		height: '80%'
+	},
+	scrollViewContainer: {
+		height: '55%',
+		width: '90%',
+		marginTop: '5%',
+		borderRadius: 10,
+		backgroundColor: colors.secondaryLight
+	},
+	scrollViewStyle: {
+		width: '90%',
+		marginTop: '4%',
+		marginBottom: '4%',
+		backgroundColor: colors.secondaryLight
+	},
+	deviceItemContainer: {
+		marginBottom: '5%',
+		borderRadius: 10,
+		backgroundColor: colors.secondaryDark
+	},
+	contentStyle: {
+		alignItems: 'center'
+	},
+	itemTextStyle: {
+		color: colors.offWhite
 	}
 };
