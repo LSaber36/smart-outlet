e3d3885632eed0862604f4dbe62220d00a5252d0
Lucas Saber <L.Saber36@gmail.com>
2022-05-19 02:13:56 -0400

    Added Realtime database live data updates

--- app/SmartOutlet/src/screens/Device.js
+++ app/SmartOutlet/src/screens/Device.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { View, Text } from 'react-native';
 import { styles, colors } from '../styles';
 import { Button } from 'react-native-elements';
+import database from '@react-native-firebase/database';
 
 export const Device = ({ navigation, route }) => {
+	const [deviceState, setDeviceState] = useState(false);
+	const [deviceID, setDeviceID] = useState(0);
+
 	const {	container, fullWidthHeight, buttonContainer, center } = styles;
 	const { textStyle, deviceInfoText, buttonView, buttonStyle } = deviceStyles;
+
+	// TO-DO: replace with Redux
 	const deviceValue = route.params.value;
 	const deviceKey = route.params.key;
+
+	// Realtime database reference object
+	const deviceStateRef = database().ref('/test');
+
+	useEffect(() => {
+		deviceStateRef.on('value', snapshot => {
+			console.log('User data: ', snapshot.val());
+			setDeviceState(snapshot.val().state);
+			setDeviceID(snapshot.val().id);
+		});
+	});
 
 	return (
 		<View style = { container }>
 			<Text style = { textStyle }> Device Page </Text>
 			<Text style = { deviceInfoText }> { deviceValue } { deviceKey } </Text>
+			<Text style = { deviceInfoText }> Device ID: { deviceID } </Text>
+			<Text style = { deviceInfoText }> Device State: { deviceState ? 'On' : 'Off' } </Text>
 			<View style = { [buttonView, center] }>
 				<Button
 					title = 'Back'
@@ -39,7 +58,7 @@ const deviceStyles = {
 	buttonView: {
 		height: '10%',
 		width: '80%',
-		marginTop: '85%'
+		marginTop: '60%'
 	},
 	buttonStyle: {
 		width: '80%',
