2ed9ac6e512be5bc8b9fe587bcf31001de9dafaf
Lucas Saber <L.Saber36@gmail.com>
2022-05-19 02:15:14 -0400

    Updated code to send new variable names to Realtime database

--- firmware/smartOutlet/smartOutlet.ino
+++ firmware/smartOutlet/smartOutlet.ino
@@ -43,19 +43,19 @@ void loop()
     blinkLED(LED);
 
     // Boolean test
-    boolResult = Firebase.setBool(fbdo, F("/test/bool"), (count % 2 == 0));
-    Serial.printf("Set bool: %s\n", boolResult ? "ok" : fbdo.errorReason().c_str());
+    boolResult = Firebase.setBool(fbdo, F("/test/state"), (count % 2 == 0));
+    Serial.printf("Set state: %s\n", boolResult ? "ok" : fbdo.errorReason().c_str());
 
-    boolResult = Firebase.getBool(fbdo, F("/test/bool"), &deviceState);
-    Serial.printf("Get bool: %s\n", boolResult ? (deviceState ? "true" : "false") : fbdo.errorReason().c_str());
+    boolResult = Firebase.getBool(fbdo, F("/test/state"), &deviceState);
+    Serial.printf("Get state: %s\n", boolResult ? (deviceState ? "true" : "false") : fbdo.errorReason().c_str());
 
 
     // Int test
-    intResult = Firebase.setInt(fbdo, F("/test/int"), count);
-    Serial.printf("Set int:  %s\n", intResult ? "ok" : fbdo.errorReason().c_str());
+    intResult = Firebase.setInt(fbdo, F("/test/id"), count);
+    Serial.printf("Set id:  %s\n", intResult ? "ok" : fbdo.errorReason().c_str());
 
-    intResult = Firebase.getInt(fbdo, F("/test/int"), &deviceID);
-    Serial.printf("Get int:  %s\n", intResult ? String(deviceID).c_str() : fbdo.errorReason().c_str());
+    intResult = Firebase.getInt(fbdo, F("/test/id"), &deviceID);
+    Serial.printf("Get id:  %s\n", intResult ? String(deviceID).c_str() : fbdo.errorReason().c_str());
 
     // Print newline for formatting
     Serial.println();
