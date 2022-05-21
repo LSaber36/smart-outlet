acd64be2fd8a937850eaf812b16d85b83b376308
Lucas Saber <L.Saber36@gmail.com>
2022-05-20 23:29:30 -0400

    Added check in case outlet id being parsed does not exist

--- app/SmartOutlet/src/screens/Device.js
+++ app/SmartOutlet/src/screens/Device.js
@@ -26,7 +26,7 @@ export const Device = ({ navigation }) => {
 		<View style = { container }>
 			<Text style = { textStyle }> Device Page </Text>
 			<Text style = { deviceInfoText }> Device ID: { deviceID } </Text>
-			<Text style = { deviceInfoText }> Device State: { deviceState ? 'On' : 'Off' } </Text>
+			<Text style = { deviceInfoText }> Device State: { (deviceState != undefined) ? (deviceState ? 'On' : 'Off') : 'Undefined' } </Text>
 			<View style = { [buttonView, center] }>
 				<Button
 					title = 'Back'
