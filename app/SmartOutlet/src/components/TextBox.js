c81c05eb3ef8e77e98a9c0f40407c15b55064226
Lucas Saber <L.Saber36@gmail.com>
2022-05-24 17:13:28 -0400

    Added formik to login page and made input component

---
+++ app/SmartOutlet/src/components/TextBox.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+import { styles, colors } from '../styles';
+
+export const TextBox = ({ header, placeholder, onChangeText, value, errorMesage }) => {
+	const {	center, input, errorText } = styles;
+	const { loginInputHeader, loginField } = textBoxStyles;
+
+	return (
+		<View style = { loginField }>
+			<Text style = { loginInputHeader }>{ header }</Text>
+			<TextInput
+				style = { input }
+				placeholder = { placeholder }
+				onChangeText = { onChangeText }
+				value = { value }
+			/>
+			<View style = { center }>
+				<Text style = { errorText }>{ errorMesage }</Text>
+			</View>
+		</View>
+	);
+};
+
+const textBoxStyles = {
+	forgotPasswordText: {
+		color: colors.secondaryDark,
+		marginLeft: '30%'
+	},
+	forgotPasswordView: {
+		marginTop: '2%'
+	},
+	registerTextView: {
+		marginTop: '5%'
+	},
+	loginFormStyle: {
+		width: '100%',
+		marginTop: '15%'
+	},
+	loginInputHeader: {
+		color: colors.dark,
+		marginLeft: '5%',
+		marginBottom: '1%'
+	},
+	loginField: {
+		width: '75%',
+		marginTop: '5%'
+	}
+};
--- app/SmartOutlet/src/screens/Login.js
+++ app/SmartOutlet/src/screens/Login.js
@@ -5,19 +5,32 @@ import {
 } from 'react-native';
 import { styles, colors } from '../styles';
 import { Button } from 'react-native-elements';
+import { TextBox } from '../components';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 
+
 const { height, width } = Dimensions.get('screen');
-const inputSchema = yup.object({
+const forgotPasswordSchema = yup.object({
 	email: yup.string()
 		.required()
 		.email()
 });
+const loginSchema = yup.object({
+	email: yup.string()
+		.required()
+		.email(),
+	password: yup.string()
+		.required()
+		.min(8)
+});
 
 export const Login = ({ navigation }) => {
 	const {	container, fullWidthHeight, buttonContainer, center, input, errorText } = styles;
-	const { textStyle, buttonView, buttonStyle, registerTextView } = loginStyles;
+	const {
+		textStyle, buttonView, buttonStyle, registerTextView,
+		loginFormStyle, forgotPasswordText, forgotPasswordView
+	} = loginStyles;
 
 	const [modalVisible, setModalVisible] = useState(false);
 
@@ -43,9 +56,9 @@ export const Login = ({ navigation }) => {
 								</Text>
 								<Formik
 									initialValues = {{ email: '' }}
-									validationSchema = { inputSchema }
+									validationSchema = { forgotPasswordSchema }
 									onSubmit = { (values, actions) => {
-										console.log('Submitted Data: ' + JSON.stringify(values));
+										console.log('Forgot Password Data: ' + JSON.stringify(values));
 										actions.resetForm();
 										setModalVisible(false);
 									} }
@@ -87,35 +100,68 @@ export const Login = ({ navigation }) => {
 	};
 
 	return (
-		<View style = { container }>
-			{ renderForgotPasswordModal() }
-			<Text style = { textStyle }> Login Page </Text>
-			<View style = { [registerTextView, center] }>
-				<Text
-					style = {{ color: colors.secondaryDark }}
-					onPress = { () => setModalVisible(true) }
+		<TouchableWithoutFeedback onPress = { Keyboard.dismiss }>
+			<View style = { container }>
+				{ renderForgotPasswordModal() }
+				<Text style = { textStyle }> Login Page </Text>
+				<Formik
+					initialValues = {{ email: '', password: '' }}
+					validationSchema = { loginSchema }
+					onSubmit = { (values, actions) => {
+						console.log('Login Data: ' + JSON.stringify(values));
+						actions.resetForm();
+						// Check if login was successful through firebase first
+						navigation.navigate('Dashboard');
+					} }
 				>
-					{ 'Forgot your password? ' }
-				</Text>
+					{ (props) => (
+						<View style = { [center, loginFormStyle] }>
+							<TextBox
+								header = 'Email'
+								placeholder = 'your.name@mail.com'
+								onChangeText = { props.handleChange('email') }
+								value = { props.values.email }
+								errorMesage = { props.touched.email && props.errors.email }
+							/>
+
+							<TextBox
+								header = 'Password'
+								placeholder = 'password'
+								onChangeText = { props.handleChange('password') }
+								value = { props.values.password }
+								errorMesage = { props.touched.password && props.errors.password }
+							/>
+
+							<View style = { [forgotPasswordView, center] }>
+								<Text
+									style = { forgotPasswordText }
+									onPress = { () => setModalVisible(true) }
+								>
+									{ 'Forgot your password? ' }
+								</Text>
+							</View>
+							<View style = { [center, buttonView] }>
+								<Button
+									title = 'Login'
+									containerStyle = { [buttonContainer, buttonStyle] }
+									buttonStyle = { fullWidthHeight }
+									onPress = { props.handleSubmit }
+								/>
+							</View>
+							<View style = { [registerTextView, center] }>
+								<Text>{ 'Don\'t have an account? ' }</Text>
+								<Text
+									style = {{ color: colors.secondaryDark }}
+									onPress = { () => navigation.navigate('Signup') }
+								>
+									Register here
+								</Text>
+							</View>
+						</View>
+					) }
+				</Formik>
 			</View>
-			<View style = { [center, buttonView] }>
-				<Button
-					title = 'Login'
-					containerStyle = { [buttonContainer, buttonStyle] }
-					buttonStyle = { fullWidthHeight }
-					onPress = { () => navigation.navigate('Dashboard') }
-				/>
-			</View>
-			<View style = { [registerTextView, center] }>
-				<Text>{ 'Don\'t have an account? ' }</Text>
-				<Text
-					style = {{ color: colors.secondaryDark }}
-					onPress = { () => navigation.navigate('Signup') }
-				>
-					Register here
-				</Text>
-			</View>
-		</View>
+		</TouchableWithoutFeedback>
 	);
 };
 
@@ -126,16 +172,27 @@ const loginStyles = {
 		paddingTop: '5%'
 	},
 	buttonView: {
-		height: '10%',
+		height: '15%',
 		width: '80%',
-		marginTop: '90%'
+		marginTop: '25%'
 	},
 	buttonStyle: {
 		width: '80%',
 		height: '80%'
 	},
+	forgotPasswordText: {
+		color: colors.secondaryDark,
+		marginLeft: '30%'
+	},
+	forgotPasswordView: {
+		marginTop: '2%'
+	},
 	registerTextView: {
 		marginTop: '5%'
+	},
+	loginFormStyle: {
+		width: '100%',
+		marginTop: '15%'
 	}
 };
 
