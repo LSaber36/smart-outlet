f105cc3a9d5d4cd3e0dd84f79ebb875945e12cff
Lucas Saber <L.Saber36@gmail.com>
2022-06-07 20:03:39 -0400

    Added profile picture section to app

--- app/SmartOutlet/src/screens/Settings.js
+++ app/SmartOutlet/src/screens/Settings.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import { View, Text, Modal, Dimensions } from 'react-native';
 import { styles, colors } from '../styles';
-import { Button } from 'react-native-elements';
-import { useSelector } from 'react-redux';
+import { Button, Avatar } from 'react-native-elements';
+import { useDispatch, useSelector } from 'react-redux';
 import auth from '@react-native-firebase/auth';
+import ImagePicker from 'react-native-image-crop-picker';
+import storage from '@react-native-firebase/storage';
+import { loadUser } from '../redux';
 
 const { height, width } = Dimensions.get('screen');
 
 export const Settings = () => {
 	const {	container, fullWidthHeight, buttonContainer } = styles;
-	const { textStyle, userDataView, userDataHeader, userData, buttonView, buttonStyle } = dashboardStyles;
+	const { textStyle, avatar, userDataView, userDataHeader, userData, buttonView, buttonStyle } = dashboardStyles;
 
 	const { activeUser } = useSelector(state => state.user);
 	const [modalVisible, setModalVisible] = useState(false);
+	const dispatch = useDispatch();
 
 	const renderConfirmLogoutModal = () => {
 		return (
@@ -50,10 +54,55 @@ export const Settings = () => {
 		);
 	};
 
+	const handleImagePicker = async () => {
+		ImagePicker
+			.openPicker({
+				width: 400,
+				height: 400,
+				cropping: true,
+				freeStyleCropEnabled: true,
+				cropperCircleOverlay: true
+			})
+			.then(image => {
+				let imageRef = storage().ref(activeUser.email + '/profileImage.png');
+
+				imageRef
+					.putFile(image.path, { contentType: 'image/jpg' })
+					.then(() => {
+						console.log('Image uploaded to firebase storage');
+
+						imageRef.getDownloadURL().then((url) => {
+							auth()
+								.currentUser
+								.updateProfile({
+									photoURL: url
+								})
+								.then(() => {
+									auth().currentUser.reload();
+									dispatch(loadUser(auth().currentUser));
+								});
+						});
+					});
+			});
+	};
+
 	return (
 		<View style = { container }>
 			{ renderConfirmLogoutModal() }
-			<Text style = { textStyle }> Account </Text>
+			<View style = { avatar }>
+				<Avatar
+					size = { 200 }
+					rounded = { true }
+					source = { (activeUser.photoURL) ? { uri: activeUser.photoURL } : require('../assets/default_profile_icon.png') }
+				>
+					<Avatar.Accessory
+						size = { 50 }
+						color = { colors.offWhite }
+						underlayColor = { colors.offWhite }
+						onPress = { () => handleImagePicker() }
+					/>
+				</Avatar>
+			</View>
 			<View style = { userDataView }>
 				<Text style = { userDataHeader }> Display Name: </Text>
 				<Text style = { userData }> { activeUser?.displayName } </Text>
@@ -78,6 +127,9 @@ const dashboardStyles = {
 		fontSize: 40,
 		marginTop: '5%'
 	},
+	avatar: {
+		marginTop: '5%'
+	},
 	userDataView: {
 		width: '65%',
 		marginTop: '5%'
@@ -98,15 +150,13 @@ const dashboardStyles = {
 	buttonView: {
 		height: '10%',
 		width: '80%',
-		marginTop: '70%',
-		flexDirection: 'row',
-		justifyContent: 'space-around',
+		marginTop: '40%',
 		alignItems: 'center'
 	},
 	buttonStyle: {
-		width: '35%',
-		height: '80%',
-		backgroundColor: 'red'
+		width: '45%',
+		height: '90%',
+		marginBottom: '5%'
 	}
 };
 
