429515d285e108edc56c7226ba50d2308f08073c
Lucas Saber <L.Saber36@gmail.com>
2022-06-21 14:54:47 -0400

    Removed accidentally comitted code since it's unfinished

--- app/SmartOutlet/src/screens/Dashboard.js
+++ app/SmartOutlet/src/screens/Dashboard.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView, PermissionsAndroid } from 'react-native';
+import React from 'react';
+import { View, Text, ScrollView } from 'react-native';
 import { styles, colors } from '../styles';
 import { Button, ListItem } from 'react-native-elements';
 import { useDispatch, useSelector } from 'react-redux';
 import { setActiveID } from '../redux';
 import { addOutlet } from '../services/outletServices';
-import { BleManager } from 'react-native-ble-plx';
 import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['new NativeEventEmitter']);
 
@@ -25,47 +24,7 @@ export const Dashboard = ({ navigation }) => {
 	} = dashboardStyles;
 
 	const { activeUserData, outletRefList } = useSelector(state => state.user);
-	const [bleConnected, setBleConnected] = useState(false);
 	const dispatch = useDispatch();
-	const manager = new BleManager();
-
-	useEffect(() => {
-		const subscription = manager.onStateChange((state) => {
-			setBleConnected(state === 'PoweredOn');
-		}, true);
-
-		return () => subscription.remove();
-	}, [manager]);
-
-	const scanDevices = () => {
-		PermissionsAndroid.request(
-			PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-			{
-				title: 'Permission Localisation Bluetooth',
-				message: 'Requirement for Bluetooth',
-				buttonNeutral: 'Later',
-				buttonNegative: 'Cancel',
-				buttonPositive: 'OK'
-			}
-		)
-			.then(() => {
-				manager.startDeviceScan(null, null, (error, device) => {
-					console.log('Scanning for devices...');
-					if (error) {
-						console.log(JSON.stringify(error));
-
-						return;
-					}
-
-					if (device)
-						console.log('Device ID: ' + device.id);
-				});
-
-				setTimeout(() => {
-					manager.stopDeviceScan();
-				}, 5000);
-			});
-	};
 
 	const renderListOrMessage = (list) => {
 		return (list != undefined && list.length > 0) ?
@@ -109,9 +68,7 @@ export const Dashboard = ({ navigation }) => {
 					containerStyle = { [buttonContainer, buttonStyle] }
 					buttonStyle = { [fullWidthHeight] }
 					onPress = { () => {
-						// addOutlet(activeUserData, outletRefList, 'Living Room');
-						if (bleConnected)
-							scanDevices();
+						addOutlet(activeUserData, outletRefList, 'Living Room');
 					} }
 				/>
 			</View>
