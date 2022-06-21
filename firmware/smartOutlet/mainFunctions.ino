10df91cad9e6df412377f41e04a3ddd25d6d2e89
Lucas Saber <L.Saber36@gmail.com>
2022-06-21 11:51:27 -0400

    Moved function to more appropriate file

--- firmware/smartOutlet/mainFunctions.ino
+++ firmware/smartOutlet/mainFunctions.ino
@@ -8,3 +8,24 @@ void blinkLED(uint8_t led)
   delay(100);
   digitalWrite(led, LOW);
 }
+
+void getButtons()
+{
+  currButtonState = digitalRead(BUTTON_PIN);
+
+  // Check for a button state edge
+  if (currButtonState != prevButtonState)
+  {
+    // If the edge is falling
+    if (currButtonState == 0)
+    {
+      relayState = !relayState;
+      Serial.print("Button status: " + currButtonState);
+      Serial.println("  Relay status: " + relayState);
+    }
+  }
+
+  digitalWrite(RELAY_PIN, relayState);
+  
+  prevButtonState = currButtonState;
+}
