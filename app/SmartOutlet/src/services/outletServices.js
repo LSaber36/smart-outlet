b832634c85652ba34815a50635280ef27ca346f0
Lucas Saber <L.Saber36@gmail.com>
2022-06-24 21:29:13 -0400

    Added historical data to cloud firestore

--- app/SmartOutlet/src/services/outletServices.js
+++ app/SmartOutlet/src/services/outletServices.js
@@ -5,7 +5,7 @@ import uuid from 'react-native-uuid';
 export const addOutlet = (activeUserData, outletRefList, newOutletName) => {
 	const newOutletId = uuid.v4();
 
-	// Add a new outlet to the outlet collection
+	// Add a new outlet to the outlet RTDB
 	database()
 		.ref('/' + newOutletId.toString())
 		.set({
@@ -14,7 +14,19 @@ export const addOutlet = (activeUserData, outletRefList, newOutletName) => {
 			data: 0
 		})
 		.then(() => {
-			console.log('Added outlet to database     (ID: ' + newOutletId + ')');
+			console.log('Added outlet to RTDB           (ID: ' + newOutletId + ')');
+		});
+
+	// Add the outlet to the outlet document
+	firestore()
+		.collection('Outlets')
+		.doc(newOutletId.toString())
+		.set({
+			name: newOutletName,
+			historicalData: []
+		})
+		.then(() => {
+			console.log('Added new outlet to firestore  (ID: ' + newOutletId + ')');
 		});
 
 	// Add the outlet to the user's outlet list
@@ -25,7 +37,7 @@ export const addOutlet = (activeUserData, outletRefList, newOutletName) => {
 			outletRefs: [...outletRefList, { id: newOutletId, name: newOutletName }]
 		})
 		.then(() => {
-			console.log('Added new outlet to account  (ID: ' + newOutletId + ')');
+			console.log('Added new outlet to account    (ID: ' + newOutletId + ')');
 		});
 };
 
@@ -38,15 +50,24 @@ export const deleteOutlet = (activeUserData, outletRefList, outletID) => {
 			outletRefs: outletRefList.filter(outletRef => outletRef.id != outletID)
 		})
 		.then(() => {
-			console.log('Removed outlet from account  (ID: ' + outletID + ')');
+			console.log('Removed outlet from account    (ID: ' + outletID + ')');
 		});
 
 	// Delete the outlet from the outlet collection
+	firestore()
+		.collection('Outlets')
+		.doc(outletID.toString())
+		.delete()
+		.then(() => {
+			console.log('Deleted outlet from firestore  (ID: ' + outletID + ')');
+		});
+
+	// Delete the outlet from the outlet RTDB
 	database()
 		.ref('/' + outletID.toString())
 		.remove()
 		.then(() => {
-			console.log('Deleted outlet from database (ID: ' + outletID + ')');
+			console.log('Deleted outlet from database   (ID: ' + outletID + ')');
 		});
 };
 
