7470cdef7682c91ce25de52bf93f05695fd3addc
Lucas Saber <L.Saber36@gmail.com>
2022-07-13 20:46:21 -0400

    Added code to interact with flash memory storage and print information with button presses for testing

--- firmware/smartOutlet/mainFunctions.ino
+++ firmware/smartOutlet/mainFunctions.ino
@@ -30,12 +30,34 @@ void getButtons()
       if (releasedPressTime <= SHORT_PRESS_TIME)
       {
         Serial.println("Short press");
+        Serial.println("Writing blanks to saved info");
+        resetSavedInfo();
+        Serial.println();
 
         relayState = !relayState;
       }
       else if (releasedPressTime <= LONG_PRESS_TIME)
       {
         Serial.println("Long Press");
+        Serial.println();
+
+        getSavedInfo(&currentSsid, &currentPass, &currentUuid);
+        Serial.printf("ssid: %s\n", currentSsid.c_str());
+        Serial.printf("pass: %s\n", currentPass.c_str());
+        Serial.printf("uuid: %s\n", currentUuid.c_str());
+        Serial.println();
+        
+        Serial.println("Putting values into saved info");
+        putSavedInfo("Death Star", "honorprose034", "6281f1d0-59e2-4682-9662-a85fad04ebf7");
+
+        Serial.println("Waiting .5 sec...");
+        delay(500);
+
+        getSavedInfo(&currentSsid, &currentPass, &currentUuid);
+        Serial.printf("ssid: %s\n", currentSsid.c_str());
+        Serial.printf("pass: %s\n", currentPass.c_str());
+        Serial.printf("uuid: %s\n", currentUuid.c_str());
+        Serial.println();
       }
     }
   }
@@ -52,4 +74,46 @@ void getButtons()
   }
   
   prevButtonState = currButtonState;
+}
+
+void getSavedInfo(String * ssid, String * password, String * uuid)
+{
+  savedInfo.begin("network-info", false);
+  *ssid = savedInfo.getString("ssid", "");
+  *password = savedInfo.getString("password", "");
+  *uuid = savedInfo.getString("uuid", "");
+  savedInfo.end();
+}
+
+void putSavedInfo(String ssid, String password, String uuid)
+{
+  savedInfo.begin("network-info", false);
+  savedInfo.putString("ssid", ssid);
+  savedInfo.putString("password", password);
+  savedInfo.putString("uuid", uuid);
+  savedInfo.end();
+}
+
+void putSsidPass(String ssid, String password)
+{
+  savedInfo.begin("network-info", false);
+  savedInfo.putString("ssid", ssid);
+  savedInfo.putString("password", password);
+  savedInfo.end();
+}
+
+void putUuid(String uuid)
+{
+  savedInfo.begin("network-info", false);
+  savedInfo.putString("uuid", uuid);
+  savedInfo.end();
+}
+
+void resetSavedInfo()
+{
+  savedInfo.begin("network-info", false);
+  savedInfo.putString("ssid", "");
+  savedInfo.putString("password", "");
+  savedInfo.putString("uuid", "");
+  savedInfo.end();
 }
