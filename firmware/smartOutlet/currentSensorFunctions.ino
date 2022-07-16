6fb3b9021a9e3316514ede41fd8ad18e32eba625
Lucas Saber <L.Saber36@gmail.com>
2022-07-15 22:24:56 -0400

    Updated formatting

--- firmware/smartOutlet/currentSensorFunctions.ino
+++ firmware/smartOutlet/currentSensorFunctions.ino
@@ -13,56 +13,54 @@ void setupADC()
     ADCInitialized = true;
   }
 
-  counter=0;
-  averageVoltage=0;
-  while (counter<50)
+  counter = 0;
+  averageVoltage = 0;
+
+  while (counter < 50)
   {
     ADCResult = ads.readADC_Differential_0_1() * ADCMultiplier/1000;
-    averageVoltage=averageVoltage+ADCResult;
-    counter=counter+1;
+    averageVoltage = averageVoltage + ADCResult;
+    counter++;
     delay(7);
   }
-  averageVoltage=averageVoltage/counter;
+  averageVoltage = averageVoltage / counter;
 }
 
 void getADCReading()
 {
    // Without this if check, calling readADC_Differential_0_1() causes an infinite loop
   if (ADCInitialized)
-  maxv=averageVoltage;
-  minv=averageVoltage;
-  long time_check = millis();
-
- while (millis() - time_check < 1000)
   {
-    ADCResult = ads.readADC_Differential_0_1() * ADCMultiplier/1000;
-    if(maxv<ADCResult)maxv=ADCResult;
-    if(minv>ADCResult)minv=ADCResult;
+    maxv = averageVoltage;
+    minv = averageVoltage;
+    long time_check = millis();
+
+    while (millis() - time_check < 1000)
+    {
+      ADCResult = ads.readADC_Differential_0_1() * ADCMultiplier / 1000;
+
+      if (maxv < ADCResult)
+        maxv = ADCResult;
+      
+      if (minv > ADCResult)
+        minv = ADCResult;
+    }
+    if ((averageVoltage - minv) > (maxv - averageVoltage))
+      maxv = averageVoltage + averageVoltage - minv;
+      
+    ADCValue = (maxv - averageVoltage) * 10;
+    
+    //Ignore small voltage differences
+    if((maxv - minv) < .1)
+      ADCValue = 0;  
+
+    Serial.printf("Voltage Reading: %.2f\n", ADCResult);
+    Serial.printf("MaxV: %.2f\n", maxv);
+    Serial.printf("MinV: %.2f\n", minv);
+    Serial.printf("Mean: %.2f\n", mean);
+    Serial.printf("IRMS Current?: %.2f\n", ADCValue);
+    Serial.println();
+
+    delay(500);
   }
-  if ((averageVoltage-minv)>(maxv-averageVoltage))maxv=averageVoltage+averageVoltage-minv;
-  ADCValue = (maxv-averageVoltage)*10;
-  
-  //Ignore small voltage differences
-  if((maxv-minv)<.1)ADCValue=0;  
-
-  Serial.print("Voltage Reading: ");
-  Serial.println(ADCResult); 
-  Serial.print("MaxV: ");
-  Serial.println(maxv); 
-
-  Serial.print("MinV: ");
-  Serial.println(minv);
-
-  Serial.print("Mean: ");
-  Serial.println(mean);
-  
-  Serial.print("IRMS Current?: ");
-  Serial.println(ADCValue); 
-
-  Serial.println();
-
-
-  
-
-  delay(500);
 } 
